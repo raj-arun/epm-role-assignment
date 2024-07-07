318f803ee703ce02f1100a9b2ebd557728a2d921
Arun Raj <s.arun.raj@gmail.com>
2024-07-07 08:15:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+st.text_input("EPM URL: ")
 st.subheader("Raw Data")
