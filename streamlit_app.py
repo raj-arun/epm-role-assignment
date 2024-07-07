99e2322bcbba598c5aa09d4c7a38d2e8c55ab85a
Arun Raj <s.arun.raj@gmail.com>
2024-07-07 08:18:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,17 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+role = st.radio(
+    "Do you have Service Adminstrator Role?",
+    ["Yes", "No"],)
+
+if role == "Yes":
+    st.write("You can use this service".)
+else:
+    st.write("You cannot user this serivice.")
+
 st.text_input("EPM URL: ")
+st.text_input("EPM Username: ")
+st.text_input("EPM Password: ")
+
 st.subheader("Raw Data")
